41b821d5bd34bd3bd479bf0f496eece2f2360c26
thefjrm <felipemartinsplayernet@gmail.com>
2022-03-07 20:12:34 -0300

    Update service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,17 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js").then(function () {
-    console.log("Service Worker Registered");
-  });
+
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    });
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
