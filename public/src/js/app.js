fc8197684ea8805f4de23914d16d1c9eb2978658
thefjrm <felipemartinsplayernet@gmail.com>
2022-03-09 18:19:07 -0300

    update service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,23 +1,22 @@
-
 var deferredPrompt;
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register("./sw.js")
     .then(function () {
-      console.log('Service worker registered!');
+      console.log("Service worker registered!");
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
   deferredPrompt = event;
   return false;
